14d645c3e899698aa8122c7325303cb06448113c
Sil <144889618+Silverbullet3078@users.noreply.github.com>
2025-10-29 12:31:12 +0100

    Session cal. refactured
    Refactor gedaan

--- MobyPark/api/session_calculator.py
+++ MobyPark/api/session_calculator.py
@@ -1,3 +1,4 @@
+from typing import List, Dict, Any
 from datetime import datetime
 from storage_utils import load_payment_data
 from hashlib import md5
@@ -5,44 +6,45 @@ import math
 import uuid
 
 def calculate_price(parkinglot, sid, data):
-    price = 0
-    start = datetime.strptime(data["started"], "%d-%m-%Y %H:%M:%S")
+    # Deze functie berekent de prijs op basis van je start en end tijd.
 
-    if data.get("stopped"):
-        end = datetime.strptime(data["stopped"], "%d-%m-%Y %H:%M:%S")
-    else:
-        end = datetime.now()
+    start_time = datetime.strptime(data["started"], "%d-%m-%Y %H:%M:%S")
+    end_time = datetime.strptime(data["stopped"], "%d-%m-%Y %H:%M:%S") if data.get("stopped") else datetime.now()
 
-    diff = end - start
-    hours = math.ceil(diff.total_seconds() / 3600)
+    duration = end_time - start_time
+    total_hours = math.ceil(duration.total_seconds() / 3600)
+    total_days = math.ceil(duration.total_seconds() / 86400)
 
-    if diff.total_seconds() < 180:
-        price = 0
-    elif end.date() > start.date():
-        price = float(parkinglot.get("daytariff", 999)) * (diff.days + 1)
-    else:
-        price = float(parkinglot.get("tariff")) * hours
+    hourly_rate = float(parkinglot.get("tariff", 0))
+    daily_rate = float(parkinglot.get("daytariff", 999))
 
-        if price > float(parkinglot.get("daytariff", 999)):
-            price = float(parkinglot.get("daytariff", 999))
+    if duration.total_seconds() < 180:  # Dus als je minder dan 3 min parked dan is het gratis 
+        price = 0.0
+    elif total_days > 0:    # als je meer dan een dag parkeerd 
+        price = daily_rate * total_days
+    else:                   # als je binnen een dag parkeerd
+        price = min(hourly_rate * total_hours, daily_rate)
+    
+    return price, total_hours, total_days
 
-    return (price, hours, diff.days + 1 if end.date() > start.date() else 0)
 
 
 
 def generate_payment_hash(sid, data):
+    # Hier generated die en md5 hash voor de betaling
     return md5(str(sid + data["licenseplate"]).encode("utf-8")).hexdigest()
 
 
 def generate_transaction_validation_hash():
     return str(uuid.uuid4())
 
-def check_payment_amount(hash):
+def check_payment_amount(tx_hash):
+    # Deze functie returned het bedrag van een transactie
+
     payments = load_payment_data()
-    total = 0
 
-    for payment in payments:
-        if payment["transaction"] == hash:
-            total += payment["amount"]
-
-    return total
+    return sum(
+        payment.get("amount", 0)                    # type: ignore
+        for payment in payments                     # type: ignore
+        if payment.get("transaction") == tx_hash    # type: ignore
+    )
