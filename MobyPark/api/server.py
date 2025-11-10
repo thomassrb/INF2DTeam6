8b805ee31e4e99be7719ae12e4358e3aabb86aa1
Wesley <124694992+wesley7886@users.noreply.github.com>
2025-11-10 02:28:46 +0100

    get routes and fix vehicle loading and saving
    get route methods moved to get_routes class.they are adjusted and ready to use replasced self.load vehicls withl oad_vehicles_data() removed the local methods load vehicle data and save vehicle data.added the imports ass well

--- MobyPark/api/server.py
+++ MobyPark/api/server.py
@@ -1,9 +1,9 @@
-# --- bootstrap so this file can be run directly or as a module ---
+""" --- bootstrap zodat dit bestand kan gerunt worden als module en gebruikt voor imports ---"""
 if __name__ == "__main__" and __package__ is None:
     import sys, pathlib
-    sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))  # add project root
+    sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
     __package__ = "api"
-# -----------------------------------------------------------------
+"""-----------------------------------------------------------------"""
 import json
 import uuid
 from datetime import datetime
@@ -11,10 +11,11 @@ import re
 import time
 import threading
 import hashlib
+from .routes.get_routes import get_routes   
 from .routes.put_routes import put_routes
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
-from storage_utils import load_json, save_data, save_user_data, load_parking_lot_data, save_parking_lot_data, save_reservation_data, load_reservation_data, load_payment_data, save_payment_data
+from storage_utils import load_json, save_data, save_user_data, load_parking_lot_data, save_parking_lot_data, save_reservation_data, load_reservation_data, load_payment_data, save_payment_data, load_vehicles_data,save_vehicles_data
 import session_calculator as sc
 import authentication
 
@@ -129,22 +130,22 @@ class RequestHandler(BaseHTTPRequestHandler):
                 '/payments/': lambda: put_routes._handle_update_payment(self, authentication.get_user_from_session(self)),
             },
             'GET': {
-                '/': self._handle_index,
-                '/index': self._handle_index,
-                '/index.html': self._handle_index,
-                '/favicon.ico': self._handle_favicon,
-                '/parking-lots': self._handle_get_parking_lots,
+                '/': lambda: get_routes._handle_index(self),
+                '/index': lambda: get_routes._handle_index(self),
+                '/index.html': lambda: get_routes._handle_index(self),
+                '/favicon.ico': lambda: get_routes._handle_favicon(self),
+                '/parking-lots': lambda: get_routes._handle_get_parking_lots(self),
                 '/profile': lambda: authentication.handle_get_profile(self, authentication.get_user_from_session(self)),
                 re.compile(r'^/profile/([^/]+)$'): lambda: authentication.handle_get_profile_by_id(self,  authentication.get_user_from_session(self)),
                 '/logout': lambda: authentication.handle_logout(self),
-                '/reservations': self._handle_get_reservations,
-                '/payments': self._handle_get_payments,
-                '/billing': self._handle_get_billing,
-                '/vehicles': self._handle_get_vehicles,
-                '/parking-lots/': self._handle_get_parking_lot_details,
-                '/reservations/': self._handle_get_reservation_details,
-                '/payments/': self._handle_get_payment_details,
-                '/billing/': self._handle_get_user_billing,
+                '/reservations': lambda: get_routes._handle_get_reservations(self),
+                '/payments':lambda: get_routes._handle_get_payments(self),
+                '/billing':lambda: get_routes._handle_get_billing(self),
+                '/vehicles':lambda: get_routes._handle_get_vehicles(self),
+                '/parking-lots/':lambda: get_routes._handle_get_parking_lot_details(self),
+                '/reservations/':lambda: get_routes._handle_get_reservation_details(self),
+                '/payments/': lambda: get_routes._handle_get_payment_details(self),
+                '/billing/': lambda: get_routes._handle_get_user_billing(self),
                 '/vehicles/': self._handle_get_vehicle_details,
                 '/vehicles/reservations': self._handle_get_vehicle_reservations,
                 '/vehicles/history': self._handle_get_vehicle_history,
@@ -394,7 +395,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             self._send_json_response(400, "application/json", error)
             return
         
-        vehicles = self._load_vehicles()
+        vehicles = load_vehicles_data()
         users = load_json('users.json')
         current_user = next((u for u in users if u.get('username') == session_user['username']), None)
         
@@ -417,7 +418,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         }
         user_vehicles.append(vehicle)
         vehicles[current_user["username"]] = user_vehicles
-        self._save_vehicles(vehicles)
+        save_vehicles_data(vehicles)
         self.audit_logger.audit(session_user, action="create_vehicle", target=new_vid, extra={"license_plate": data['licenseplate']})
         self._send_json_response(201, "application/json", {"status": "Success", "vehicle": vehicle})
 
@@ -583,8 +584,8 @@ class RequestHandler(BaseHTTPRequestHandler):
     @login_required
     def _handle_delete_vehicle(self, session_user):
         vid = self.path.replace("/vehicles/", "")
-        
-        vehicles = self._load_vehicles()
+
+        vehicles = load_vehicles_data()
         user_vehicles = vehicles.get(session_user["username"])
         
         if not user_vehicles:
@@ -602,32 +603,6 @@ class RequestHandler(BaseHTTPRequestHandler):
         save_data("vehicles.json", vehicles)
         self.audit_logger.audit(session_user, action="delete_vehicle", target=vid)
         self._send_json_response(200, "application/json", {"status": "Deleted"})
-
-    def _handle_index(self):
-        self._send_json_response(200, "text/html; charset=utf-8", 
-            "<html><head><title>MobyPark API</title></head>"
-            "<body>"
-            "<h1>MobyPark API is running</h1>"
-            "<p>Try endpoints like <code>/parking-lots</code>, <code>/profile</code> (requires Authorization), etc.</p>"
-            "</body></html>"
-        )
-
-    def _handle_favicon(self):
-        self._send_json_response(204, "image/x-icon", "")
-
-    def _handle_get_parking_lots(self):
-        parking_lots = load_parking_lot_data()
-        self._send_json_response(200, "application/json", parking_lots)
-
-    def _handle_get_parking_lot_details(self):
-        lid = self.path.split("/")[2]
-        parking_lots = load_parking_lot_data()
-        
-        if lid not in parking_lots:
-            self._send_json_response(404, "application/json", {"error": "Parking lot not found"})
-            return
-
-        self._send_json_response(200, "application/json", parking_lots[lid])
 
     @login_required 
     def _handle_get_parking_lot_sessions(self, session_user):
@@ -659,129 +634,6 @@ class RequestHandler(BaseHTTPRequestHandler):
             self._send_json_response(200, "application/json", sessions[sid])
 
     @login_required
-    def _handle_get_reservations(self, session_user):
-        reservations = load_reservation_data()
-        print(f"DEBUG: In _handle_get_reservations. Session User: {session_user}")
-        print(f"DEBUG: Raw Reservations Data: {reservations}")
-        user_reservations = {rid: res for rid, res in reservations.items() if res.get("user") == session_user["username"] or session_user["role"] == "ADMIN"}
-        self._send_json_response(200, "application/json", user_reservations)
-
-    @login_required
-    def _handle_get_reservation_details(self, session_user):
-        reservations = load_reservation_data()
-        rid = self.path.replace("/reservations/", "")
-        
-        if rid not in reservations:
-            self._send_json_response(404, "application/json", {"error": "Reservation not found"})
-            return
-                
-
-        
-        if not (session_user["role"] == "ADMIN") and not session_user["username"] == reservations[rid].get("user"):
-            self._send_json_response(403, "application/json", {"error": "Access denied"})
-            return
-        
-        self._send_json_response(200, "application/json", reservations[rid])
-
-    @login_required
-    def _handle_get_payments(self, session_user):
-        payments = []
-        for payment in load_payment_data():
-            if payment.get("initiator") == session_user["username"] or payment.get("processed_by") == session_user["username"] or session_user["role"] == "ADMIN":
-                payments.append(payment)
-        self._send_json_response(200, "application/json", payments)
-
-    @login_required
-    def _handle_get_payment_details(self):
-        session_user = authentication.get_user_from_session(self)
-        pid = self.path.replace("/payments/", "")
-        payments = load_payment_data()
-        payment = next((p for p in payments if p.get("transaction") == pid), None)
-        if not payment:
-            self._send_json_response(404, "application/json", {"error": "Payment not found!"})
-            return
-        if not (session_user["role"] == "ADMIN") and payment.get("initiator") != session_user["username"]:
-            self._send_json_response(403, "application/json", {"error": "Access denied"})
-            return
-        self._send_json_response(200, "application/json", payment)
-
-    @login_required
-    def _handle_get_billing(self, session_user):
-        data = []
-        for pid, parkinglot in load_parking_lot_data().items():
-            try:
-                sessions = load_json(f'pdata/p{pid}-sessions.json')
-            except FileNotFoundError:
-                sessions = {}
-            for sid, session in sessions.items():
-                if session["user"] == session_user["username"]:
-                    amount, hours, days = sc.calculate_price(parkinglot, sid, session)
-                    transaction = sc.generate_payment_hash(sid, session)
-                    payed = sc.check_payment_amount(transaction)
-                    data.append({
-                        "session": {k: v for k, v in session.items() if k in ["licenseplate", "started", "stopped"]} | {"hours": hours, "days": days},
-                        "parking": {k: v for k, v in parkinglot.items() if k in ["name", "location", "tariff", "daytariff"]},
-                        "amount": amount,
-                        "thash": transaction,
-                        "payed": payed,
-                        "balance": amount - payed
-                    })
-        self._send_json_response(200, "application/json", data)
-
-    def _load_vehicles(self):
-        vehicles = load_json("vehicles.json")
-        if isinstance(vehicles, dict):
-            return vehicles
-        return {}
-
-    def _save_vehicles(self, vehicles):
-        save_data("vehicles.json", vehicles)
-
-    
-    @roles_required(['ADMIN'])
-    def _handle_get_user_billing(self, session_user):
-        
-        user = self.path.replace("/billing/", "")
-        data = []
-        for pid, parkinglot in load_parking_lot_data().items():
-            try:
-                sessions = load_json(f'pdata/p{pid}-sessions.json')
-            except FileNotFoundError:
-                sessions = {}
-            for sid, session in sessions.items():
-                if session["user"] == session_user["username"]:
-                    amount, hours, days = sc.calculate_price(parkinglot, sid, session)
-                    transaction = sc.generate_payment_hash(sid, session)
-                    payed = sc.check_payment_amount(transaction)
-                    data.append({
-                        "session": {k: v for k, v in session.items() if k in ["licenseplate", "started", "stopped"]} | {"hours": hours, "days": days},
-                        "parking": {k: v for k, v in parkinglot.items() if k in ["name", "location", "tariff", "daytariff"]},
-                        "amount": amount,
-                        "thash": transaction,
-                        "payed": payed,
-                        "balance": amount - payed
-                    })
-        self.audit_logger.audit(session_user, action="get_user_billing", target=session_user["username"])
-        self._send_json_response(200, "application/json", data)
-
-    @login_required
-    def _handle_get_vehicles(self, session_user):
-        vehicles_data = self._load_vehicles()
-
-        if session_user["role"] == "ADMIN":
-            all_vehicles = []
-            for user_v_list in vehicles_data.values():
-                all_vehicles.extend(user_v_list)
-            self._send_json_response(200, "application/json", all_vehicles)
-            return
-        else:
-            user_vehicles = vehicles_data.get(session_user["username"], [])
-            if not user_vehicles:
-                self._send_json_response(404, "application/json", {"error": "No vehicles found for this user"})
-                return
-            self._send_json_response(200, "application/json", user_vehicles)
-
-    @login_required
     def _handle_get_vehicle_reservations(self, session_user):
         vid = self.path.split("/")[2]
         
@@ -794,8 +646,8 @@ class RequestHandler(BaseHTTPRequestHandler):
             else:
                 self._send_json_response(400, "application/json", {"error": "Invalid vehicle reservations request"})
                 return
-        
-        vehicles = self._load_vehicles()
+
+        vehicles = load_vehicles_data()
         user_vehicles = vehicles.get(target_user)
         
         if not user_vehicles:
@@ -918,7 +770,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             self._send_json_response(400, "application/json", {"error": "Invalid URL format for vehicle details"})
             return
 
-        vehicles_data = self._load_vehicles()
+        vehicles_data = load_vehicles_data()
         user_vehicles = vehicles_data.get(target_username, [])
         
         if not user_vehicles:
@@ -944,7 +796,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         # Cleared payment data
         save_payment_data([])
         # Cleared de voertuigen data
-        self._save_vehicles({})
+        save_vehicles_data({})
         # Cleared de huidige sessions
         self.session_manager.active_sessions.clear()
 
