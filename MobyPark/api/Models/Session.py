ddd40ee8edac9c122cd1960fb5de8258f6799634
ArdyVisser <1061327@hr.nl>
2025-11-24 20:45:41 +0100

    git opgekut dus dit is oplossing

--- MobyPark/api/Models/Session.py
+++ MobyPark/api/Models/Session.py
@@ -1,24 +1,31 @@
 from datetime import datetime
+from .Vehicle import Vehicle
+from .ParkingLot import ParkingLot
+from .User import User
 
 class Session:
 
     def __init__(self,
                  id: str,
-                 parking_lot_id: str,
-                 vehicle_id: str,
+                 parking_lot: ParkingLot,
+                 vehicle: Vehicle,
+                 licenseplate: str,
                  started: datetime,
                  stopped: datetime,
-                 user_id: str,
+                 user: User,
+                 username: str,
                  duration_minutes: int,
                  cost: float,
                  payment_status: str):
         
         self.id = id
-        self.parking_lot_id = parking_lot_id
-        self.vehicle_id = vehicle_id
+        self.parking_lot = parking_lot
+        self.vehicle = vehicle
+        self.licenseplate = licenseplate
         self.started = started
         self.stopped = stopped
-        self.user_id = user_id
+        self.user = user
+        self.username = username
         self.duration_minutes = duration_minutes
         self.cost = cost
         self.payment_status = payment_status
